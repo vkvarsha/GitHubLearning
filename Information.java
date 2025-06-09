a02eba5aeb01dc07801a53a25de0ab5e30e7620f
Varsha Kadam <varsha.kadam27@yahoo.com>
2025-06-09 21:31:44 +0530

    My first deployement

---
+++ Information.java
@@ -0,0 +1,32 @@
+package start;
+
+public class Information {
+
+	public static void main(String[] args)
+	{
+		
+		/*  
+		 * Core Jave + Selenium
+		 * Core java is a programming language
+		 * Selenium -- is Automation tool which can only Automate only web based application
+		 * You cannot automate a mobile based application
+		 * you cannot automate stand alone application (Not require internet)
+		 * Core Jave -- Editor tools
+		 * 
+		 * Core Jave -- Editor tools -- Eclipse -- Oxygen version
+		 * Go to google and search -> eclipse oxygen free download
+		 * go to first one --> Eclipse IDE for Java EE Developers
+		 * Download the .exe file  
+		 * 
+		 * 
+		 * Below are the extensions of Builds
+		 * .exe,.jar,.rar,.tar,.apk,.zip
+		 * 
+		 * 
+		 * 
+		 * */
+		
+		
+	}
+
+}
